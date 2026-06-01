44ab23f24f258ac19a7685eb2fce1e2be3a3a4c3
Ricardo López Reyero <68564272+ricardolopezreyero@users.noreply.github.com>
2026-06-01 11:26:49 -0600

    update rayosx_app.py

---
+++ rayosx_app.py
@@ -0,0 +1,6 @@
+"""Entry point for Rayos X Inscripciones."""
+import uvicorn
+from app.main import app
+
+if __name__ == "__main__":
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8765, reload=True)
